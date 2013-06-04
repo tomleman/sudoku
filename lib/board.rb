74d4e2309ede0db6460abb1a29e0ba8bcd906739
James Lieu <j.lieu888@gmail.com>
2013-06-04 14:30:22 +0100

    improved code and test, status = board,row,column

--- lib/board.rb
+++ lib/board.rb
@@ -1,37 +1,47 @@
 class Board
 
-	def input
+	def initialize
 		@input = "409075310130600807005200096016009720080020606007430000500107040720048009603050008"
-		
 	end
 
-	def input_count
-		raise "Needs the length to be 81" if input.length != 81
-		@input.length
-		
-	end
 
 	def grid
-
-		@grid = input.split("").map {|i| i.to_i}.each_slice(9).to_a
+		@grid = (0..80).to_a
 		
-			@grid.map {|num| 
-				num = num.each_slice(3).to_a}
 	end
 
-	def row	
-		grid.map {|i| i.join}
+	def cell
+		@cell 
 	end
+
+
+	def row
+
+		row_array = grid.each_slice(9).to_a
+
+		
+	end
+
+	def column
+		@columns = (0..8).map do |col|
+			@grid.select{|i| i % 9 == col}
+		end
+	end
+
+
+	
+end
+
+# first element, of first 3 elements in board make up box-1
 
 # def box
-# 	board.each do |e| 
-# 		e.map {|i| i = i.first | }
-# end
-# puts box
+# 	grid.each do |e| 
+# 		e.map {|i| i = [i][i].first }
+# 	end
 # end
 
-	def singlecell
-		cell = nil
-	end
-end
+# 	def singlecell
+# 		cell = nil
+# 	end
+# end
 
--- test/board_test.rb
+++ test/board_test.rb
@@ -5,32 +5,43 @@ class BoardTest < MiniTest::Unit::TestCase
 
 	def setup
 		@board = Board.new
-		
+		@row = @board.row
+		@column = @board.column
+
 	end
 
 	def test_board_row
+		
 		puts @board.grid.inspect
-		puts @board.row.inspect
-		puts @board.input
+		puts @row.inspect
+		puts @column.inspect
+		# puts @board.input
+		puts @board.grid.length.inspect
+
 	end
 
 	def test_input_contains_81_cells
-		assert_equal 81, @board.input_count
+		assert_equal 81, @board.grid.length
 	end
 
-	def test_the_runtime_error_for_when_not_enough_numbers_are_input
-		assert_raises RuntimeError do 
-			raise 'error' 
-			@board.input_count < 81
-		end
-	end
+	# def test_the_runtime_error_for_when_not_enough_numbers_are_input
+	# 	assert_raises RuntimeError do 
+	# 		raise 'error' 
+	# 		@board.input_count < 81
+	# 	end
+	# end
 
 	def test_row_number_are_9_charaters_long
-		assert_equal @board.row.length, 9
+		assert_equal @row.length, 9
 	end
 
-	def method_name
-		
-	end
+	# def test_column_length
+	# 	assert_equal @column.length, 9
+	# end
+	# end
+	# def test_box_contains_1_to_9
+	# 	assert @board.box, [1..9] 
+
+	# end
 
 end
