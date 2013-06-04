46f27a3bd312748b566933e5754a7dfd747b9ce2
James Lieu <j.lieu888@gmail.com>
2013-06-04 14:46:15 +0100

    commiting test

--- test/board_test.rb
+++ test/board_test.rb
@@ -24,21 +24,15 @@ class BoardTest < MiniTest::Unit::TestCase
 		assert_equal 81, @board.grid.length
 	end
 
-	# def test_the_runtime_error_for_when_not_enough_numbers_are_input
-	# 	assert_raises RuntimeError do 
-	# 		raise 'error' 
-	# 		@board.input_count < 81
-	# 	end
-	# end
-
-	def test_row_number_are_9_charaters_long
+	def test_row_number_are_9_characters_long
 		assert_equal @row.length, 9
 	end
 
-	# def test_column_length
-	# 	assert_equal @column.length, 9
-	# end
-	# end
+	def test_column_length
+		assert_equal @column.length, 9
+	end
+
+
 	# def test_box_contains_1_to_9
 	# 	assert @board.box, [1..9] 
 
